9aac5e899ce00df435226cbd30c0949fb913083b
Tony G. Bolaño <tonybolanyo@gmail.com>
2018-05-15 14:37:13 +0200

    Image is optional and no loging required for posting

--- backend/uploader/models.py
+++ backend/uploader/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class UploadedFile(models.Model):
 
     name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='uploads/%Y/%m/%d/')
+    image = models.ImageField(upload_to='uploads/%Y/%m/%d/', blank=True, null=True)
 
     def __str__(self):
         return self.name
