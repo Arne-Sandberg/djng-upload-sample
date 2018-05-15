ee1e5ea112e050ee55ad6ebbd602bdab4abb0d3b
Tony G. Bolaño <tonybolanyo@gmail.com>
2018-05-15 12:24:04 +0200

    Add submit function to frontend app

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { UploaderComponent } from './uploader/uploader.component';
@@ -12,7 +12,8 @@ import { UploaderComponent } from './uploader/uploader.component';
     UploaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/uploader/uploader.component.ts
+++ frontend/src/app/uploader/uploader.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-uploader',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploaderComponent implements OnInit {
 
-  constructor() { }
+  files: File[];
+  name: string;
+
+  constructor(private _http: HttpClient) { }
 
   ngOnInit() {
+  }
+
+  onInputFileChange(event) {
+    // the files list is in event.target.files
+    // grab it when change, before form submit
+    this.files = <File[]>event.target.files;
+    console.log(this.files);
+  }
+
+  onSubmit() {
+    const formData = new FormData();
+    formData.append('name', this.name);
+    formData.append('image', this.files[0], this.files[0].name);
+    this._http
+      .post('http://localhost:8000/api/1.0/files/', formData)
+      .subscribe(response => {
+        console.log(response);
+      }, error => {
+        console.error(error);
+      });
   }
 
 }
