45ec70818c51c01a7561a5173ca588c9b5af374d
Tony G. Bolaño <tonybolanyo@gmail.com>
2018-05-15 14:39:19 +0200

    Add reactive form and POST API call. API calls should be on service.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UploaderComponent } from './uploader/uploader.component';
@@ -13,7 +14,8 @@ import { UploaderComponent } from './uploader/uploader.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/uploader/uploader.component.ts
+++ frontend/src/app/uploader/uploader.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -9,30 +10,48 @@ import { HttpClient } from '@angular/common/http';
 export class UploaderComponent implements OnInit {
 
   files: File[];
-  name: string;
+  theForm: FormGroup;
 
-  constructor(private _http: HttpClient) { }
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _http: HttpClient
+  ) {
+    this._createForm();
+  }
 
   ngOnInit() {
+  }
+
+  private _createForm() {
+    this.theForm = this._formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(5)]],
+      image: ''
+    });
   }
 
   onInputFileChange(event) {
     // the files list is in event.target.files
     // grab it when change, before form submit
     this.files = <File[]>event.target.files;
-    console.log(this.files);
+    console.log('File selected:', this.files);
   }
 
   onSubmit() {
-    const formData = new FormData();
-    formData.append('name', this.name);
-    formData.append('image', this.files[0], this.files[0].name);
+    const values = this.theForm.value;
+    const formData = new FormData(this.theForm.value);
+    formData.append('name', values.name);
+    if (this.files && this.files.length > 0) {
+      formData.append('image', this.files[0], this.files[0].name);
+    }
     this._http
       .post('http://localhost:8000/api/1.0/files/', formData)
       .subscribe(response => {
-        console.log(response);
+        console.log('saved!', response);
+        alert('Saved!');
+        this.theForm.reset();
       }, error => {
         console.error(error);
+        alert('error\n' + error);
       });
   }
 
